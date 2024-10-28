eb23409dff6a8a6bccea30745dc1381b05c471d9
sv022 <ssvatkin04@gmail.com>
2024-10-28 09:49:07 +0300

    Revert "add: services cart page"
    This reverts commit 0f1a7f87622be3eb4a3a8791980b8571b37f950b.

--- AidStore/lib/pages/cart_page.dart
+++ AidStore/lib/pages/cart_page.dart
@@ -1,18 +1,19 @@
-import 'package:aidstore/models/services.dart';
+import 'package:aidstore/pages/add_listing_page.dart';
 import 'package:flutter/material.dart';
-import '../components/service_cart_card.dart';
+import '../components/listing_cart_card.dart';
 import '../components/bottom_menu.dart';
+import '../models/listing.dart';
 
 
-List<Service> getUniqueServices(){
-    List<Service> uniqueServices = [];
+List<Listing> getUniqueListings(){
+    List<Listing> uniqueListings = [];
 
-    for (Service l in serviceCart){
-      if (!uniqueServices.contains((l))){
-        uniqueServices.add(l);
+    for (Listing l in listingCart){
+      if (!uniqueListings.contains((l))){
+        uniqueListings.add(l);
       }
     }
-    return uniqueServices;
+    return uniqueListings;
 }
 
 class CartPage extends StatefulWidget {
@@ -23,32 +24,43 @@ class CartPage extends StatefulWidget {
 }
 
 class CartPageState extends State<CartPage> {
-  final int selectedIndex = 1;
-  List<Service> uniqueServices = getUniqueServices();
+  final int selectedIndex = 2;
+  List<Listing> uniqueListings = getUniqueListings();
 
   @override
   Widget build(BuildContext context) {
 
     return Scaffold(
       appBar: AppBar(
-        toolbarHeight: 128.0,
-        title: const Padding(
-          padding: EdgeInsets.symmetric(horizontal: 16.0),
-          child: Text(
-            'Корзина',
-            style: TextStyle(fontWeight: FontWeight.w600),
-          ),
+        title: Image.network(
+          'https://raw.githubusercontent.com/sv022/MusicTrade/refs/heads/main/images/guitar_logo.png',
+          height: 50,
         ),
       ),
       backgroundColor: Colors.white,
       body: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: ListView.builder(
-          itemCount: uniqueServices.length,
+        child: GridView.builder(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            childAspectRatio: 1.45 / 2,
+            mainAxisSpacing: 2,
+          ),
+          itemCount: uniqueListings.length,
           itemBuilder: (BuildContext context, int index) {
-            return ServiceCartCard(service: uniqueServices[index]);
+            return ListingCartCard(listing: uniqueListings[index]);
           },
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => const AddListingPage()),
+          )
+        },
+        tooltip: 'Add Note',
+        child: const Icon(Icons.add),
       ),
       bottomNavigationBar: BottomMenu(selectedIndex: selectedIndex),
     );

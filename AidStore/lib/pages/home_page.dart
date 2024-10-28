d4bd38e72130e6170f0bddb34af31f9ef8c7e3bb
sv022 <ssvatkin04@gmail.com>
2024-10-28 09:49:22 +0300

    Revert "add: services info & main page"
    This reverts commit d58baa855d23b1b0918e4d9609ae6c9a7ce31973.

--- AidStore/lib/pages/home_page.dart
+++ AidStore/lib/pages/home_page.dart
@@ -1,7 +1,8 @@
-import 'package:aidstore/models/services.dart';
+import 'package:aidstore/pages/add_listing_page.dart';
 import 'package:flutter/material.dart';
-import '../components/service_card.dart';
+import '../components/listing_card.dart';
 import '../components/bottom_menu.dart';
+import '../models/listing.dart';
 
 
 class HomePage extends StatefulWidget {
@@ -18,24 +19,35 @@ class HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        toolbarHeight: 128.0,
-        title: const Padding(
-          padding: EdgeInsets.symmetric(horizontal: 16.0),
-          child: Text(
-            'Каталог услуг',
-            style: TextStyle(fontWeight: FontWeight.w600),
-          ),
+        title: Image.network(
+          'https://raw.githubusercontent.com/sv022/MusicTrade/refs/heads/main/images/guitar_logo.png',
+          height: 50,
         ),
       ),
       backgroundColor: Colors.white,
       body: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: ListView.builder(
-          itemCount: services.length,
+        child: GridView.builder(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            childAspectRatio: 1.55 / 2,
+            mainAxisSpacing: 2,
+          ),
+          itemCount: listings.length,
           itemBuilder: (BuildContext context, int index) {
-            return ServiceCard(service: services[index]);
+            return ListingCard(listing: listings[index]);
           },
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => const AddListingPage()),
+          )
+        },
+        tooltip: 'Add Note',
+        child: const Icon(Icons.add),
       ),
       bottomNavigationBar: BottomMenu(selectedIndex: selectedIndex),
     );
